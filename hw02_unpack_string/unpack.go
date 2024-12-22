e776aac268e29d9539e299160582c64989e6a51b
Maxim Ishchenko <maishchenko@sberautotech.ru>
2024-12-22 23:59:16 +0300

    hw2 is complitted

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,73 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func IsLetterOrNewString(r rune) bool {
+	return unicode.IsLetter(r) || string(r) == "\n"
+}
+
+func IsContainAllowedSymbols(inputStr string) bool {
+	for _, r := range inputStr {
+		isLetter := IsLetterOrNewString(r)
+		isDigit := unicode.IsDigit(r)
+		if !isLetter && !isDigit {
+			return false
+		}
+	}
+	return true
+}
+
+func Unpack(inputStr string) (string, error) {
+	if inputStr == "" {
+		return "", nil
+	}
+	if !IsContainAllowedSymbols(inputStr) {
+		return "", ErrInvalidString
+	}
+
+	var sb strings.Builder
+
+	var curSymb, prevSymb rune
+
+	for idx, r := range inputStr {
+		if idx == 0 {
+			if unicode.IsDigit(r) {
+				return "", ErrInvalidString
+			}
+			prevSymb = r
+			continue
+		}
+
+		curSymb = r
+		switch {
+		case IsLetterOrNewString(prevSymb) && IsLetterOrNewString(curSymb):
+			sb.WriteString(string(prevSymb))
+			prevSymb = curSymb
+		case IsLetterOrNewString(prevSymb) && unicode.IsDigit(curSymb):
+			count, err := strconv.Atoi(string(curSymb))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			for ; count > 0; count-- {
+				sb.WriteString(string(prevSymb))
+			}
+			prevSymb = curSymb
+		case unicode.IsDigit(prevSymb) && IsLetterOrNewString(curSymb):
+			prevSymb = curSymb
+		case unicode.IsDigit(prevSymb) && unicode.IsDigit(curSymb):
+			return "", ErrInvalidString
+		}
+	}
+
+	if IsLetterOrNewString(curSymb) {
+		sb.WriteString(string(curSymb))
+	}
+
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -4,8 +4,54 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint
 )
+
+func TestIsLetterOrNewString(t *testing.T) {
+	tests := []struct {
+		input    rune
+		expected bool
+	}{
+		{input: rune('d'), expected: true},
+		{input: rune('\n'), expected: true},
+		{input: rune('5'), expected: false},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(string(tc.input), func(t *testing.T) {
+			result := IsLetterOrNewString(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestIsContainAllowedSymbols(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{input: "abcd", expected: true},
+		{input: "abcd12345", expected: true},
+		{input: "12345", expected: true},
+		{input: "a1b2c3d4e5", expected: true},
+		{input: "", expected: true},
+		{input: "abcd\n", expected: true},
+		{input: "\n\n\n\n\n", expected: true},
+		{input: "abcd!", expected: false},
+		{input: "!ab\ncd!", expected: false},
+		{input: "abcd.", expected: false},
+		{input: "!", expected: false},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result := IsContainAllowedSymbols(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
@@ -16,6 +62,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "d\n5abc\n", expected: "d\n\n\n\n\nabc\n"},
+		{input: "d\n5abc\n3", expected: "d\n\n\n\n\nabc\n\n\n"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +83,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "aa!d5"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
