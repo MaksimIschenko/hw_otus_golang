06d5b6e9c9b5778ba5013fb07f2a0a470d516159
Maxim Ishchenko <maishchenko@sberautotech.ru>
2025-02-27 23:29:22 +0300

    Add tests executor

--- hw08_envdir_tool/executor/executor_test.go
+++ hw08_envdir_tool/executor/executor_test.go
@@ -1,7 +1,79 @@
 package executor
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
 
-func TestRunCmd(t *testing.T) {
-	// Place your code here
+	"github.com/MaksimIschenko/hw_otus_golang/hw08_envdir_tool/envreader"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRunCmd_Success(t *testing.T) {
+	env := envreader.Environment{
+		"TEST_ENV": {Value: "Hello", NeedRemove: false},
+	}
+
+	var stdout, stderr bytes.Buffer
+
+	oldStdout := os.Stdout
+	oldStderr := os.Stderr
+	rStdout, wStdout, _ := os.Pipe()
+	rStderr, wStderr, _ := os.Pipe()
+	os.Stdout = wStdout
+	os.Stderr = wStderr
+	defer func() {
+		os.Stdout = oldStdout
+		os.Stderr = oldStderr
+		wStdout.Close()
+		wStderr.Close()
+	}()
+
+	go func() {
+		io.Copy(&stdout, rStdout)
+	}()
+	go func() {
+		io.Copy(&stderr, rStderr)
+	}()
+
+	cmd := []string{"echo", "Hello World"}
+	exitCode := RunCmd(cmd, env)
+
+	require.Equal(t, OK, exitCode)
+	require.Contains(t, stdout.String(), "Hello World")
+}
+
+func TestRunCmd_Failure(t *testing.T) {
+	env := envreader.Environment{
+		"TEST_ENV": {Value: "Hello", NeedRemove: false},
+	}
+
+	var stdout, stderr bytes.Buffer
+
+	oldStdout := os.Stdout
+	oldStderr := os.Stderr
+	rStdout, wStdout, _ := os.Pipe()
+	rStderr, wStderr, _ := os.Pipe()
+	os.Stdout = wStdout
+	os.Stderr = wStderr
+	defer func() {
+		os.Stdout = oldStdout
+		os.Stderr = oldStderr
+		wStdout.Close()
+		wStderr.Close()
+	}()
+
+	go func() {
+		io.Copy(&stdout, rStdout)
+	}()
+	go func() {
+		io.Copy(&stderr, rStderr)
+	}()
+
+	cmd := []string{"nonexistent_command"}
+	exitCode := RunCmd(cmd, env)
+
+	require.Equal(t, ERR, exitCode)
+	require.Empty(t, stdout.String())
 }
--- hw08_envdir_tool/go.mod
+++ hw08_envdir_tool/go.mod
@@ -3,3 +3,10 @@ module github.com/MaksimIschenko/hw_otus_golang/hw08_envdir_tool
 go 1.22.1
 
 require github.com/spf13/pflag v1.0.6
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
